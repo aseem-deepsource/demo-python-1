6ece38a46bf1e92f05022bc2093e9c97b6f329b2
Aseem Manna <112926373+aseem-deepsource@users.noreply.github.com>
2023-08-22 19:10:54 +0530

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,6 +1,6 @@
 (*FIRST,) = [1, 2, 3]  # documenting code no fun
 *FIRST, a, b = [1, 2, 3]  # documenting code no fun
-a, b = [1, 8, 2, 3]  # documenting code no fun
+a, b = [1, 8, 2, 3]  # documenting code no fun hehehe
 c = 1
 
 def not_covered():
