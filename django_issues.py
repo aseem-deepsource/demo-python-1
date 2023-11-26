065cfef6999e2b42df324b9d90004d1abc339dcc
Aseem Manna <112926373+aseem-deepsource@users.noreply.github.com>
2023-11-26 14:41:46 +0530

    Update django_issues.py

--- django_issues.py
+++ django_issues.py
@@ -8,6 +8,6 @@ from django.views.decorators.http import require_http_methods
 @require_http_methods(["GET", "POST"])  # Not Sensitive?????
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = "<html><body>It is is %s.</body></html>" % now
+    html = "<html><body>It is is is %s.</body></html>" % now
     x = 1
     return HttpResponse(html)
