98e642a92b65c10cd675bb152f189aecc2b9cfdb
Aseem Manna <112926373+aseem-deepsource@users.noreply.github.com>
2024-05-27 15:35:30 +0530

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,4 +1,4 @@
-(*FIRST,) = [1, 2, 3]  # documenting code no fun
+(*FIRST,) = [1, 2, 3]  # documenting code no fungggg
 *FIRST, a, b = [1, 2, 3, 7]  # documenting code no fuddddnffffff
 a, b = [1, 8, 2, 3, 5, 6,7, 8, 9, 12, 88]  # documenting code no fun hehehehehefffffddddd
 c = 1
